a988c2e5b3bf98c34e50fbf1b1ad65242d7d680b
CATcitc <2402266953@qq.com>
2025-06-11 00:55:36 +0800

    Update WeatherConfig.java

--- src/main/java/com/pt/vx/config/WeatherConfig.java
+++ src/main/java/com/pt/vx/config/WeatherConfig.java
@@ -9,19 +9,19 @@ public class WeatherConfig {
      * 天气相关的总开关
      * true则为开启，false则为关闭
      */
-    public static boolean OPEN = false;
+    public static boolean OPEN = true;
 
     /**
      * 天气资源的key
      */
-    public static final String weatherSourceKey = "your key";
+    public static final String weatherSourceKey = "83471eb64f11cce279257698310a9e5e";
 
     /**
      * 天气资源类型
      * 0 为高德地图
      * 1 为和风天气
      */
-    public static Integer weatherSourceType = 1;
+    public static Integer weatherSourceType = 0;
 
     /**
      * 天气类型
@@ -29,7 +29,7 @@ public class WeatherConfig {
      * 1 为天气预报
      * 默认天气预报
      */
-    public static Integer getWeatherType = 1;
+    public static Integer getWeatherType = 0;
 
 
 }
